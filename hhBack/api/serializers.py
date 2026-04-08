8136c5e3894888e5e910ac7656fc9a8756968113
nurislam0777 <nu_bekbolat@kbtu.kz>
2026-04-08 11:19:24 +0500

    Add files via upload

--- hhBack/api/serializers.py
+++ hhBack/api/serializers.py
@@ -1,16 +1,18 @@
 from rest_framework import serializers
-from .models import Company, Vacancy
-
+from .models import *
 
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
         fields = '__all__'
 
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ['id', 'name']
 
 class VacancySerializer(serializers.ModelSerializer):
+    company = CompanySerializer()
     class Meta:
         model = Vacancy
-        fields = '__all__'
-
-
+        fields = ['id', 'name', 'desc', 'salary', 'company']
--- hhBack/api/urls.py
+++ hhBack/api/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
+from .viewsDef import *
+from rest_framework.routers import DefaultRouter
 from .views import *
-urlpatterns = [
-    path('/companies/', CompaniesList, name='companies-list'),
-    path('/companies/<int:id>/', CompanyDetail, name='company-detail'),
-    path('/companies/<int:id>/vacancies/', CompanyVacancies, name='company-vacancies'),
-    path('/vacancies/', VacancyList, name='vacancies-list'),
-    path('/vacancies/<int:id>/', VacancyDetail, name='vacancy-detail'),
-    path('/vacancies/top_ten/', TopTenVacancies, name='top-10-vacancies')
 
-]
+
+router = DefaultRouter()
+router.register(r'companies', CompanyViewSet)
+router.register(r'vacancies', VacancyViewSet)
+
+urlpatterns = router.urls
+
+
+# urlpatterns = [
+#     path('/companies/', CompaniesList, name='companies-list'),
+#     path('/companies/<int:id>/', CompanyDetail, name='company-detail'),
+#     path('/companies/<int:id>/vacancies/', CompanyVacancies, name='company-vacancies'),
+#     path('/vacancies/', VacancyList, name='vacancies-list'),
+#     path('/vacancies/<int:id>/', VacancyDetail, name='vacancy-detail'),
+#     path('/vacancies/top_ten/', TopTenVacancies, name='top-10-vacancies')
+# ]
